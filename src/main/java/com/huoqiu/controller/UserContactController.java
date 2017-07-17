96b01b062b1eeb6b1ce7ddc9fe592d491dfd40ed
hqtest <hqtest@zenghaiyun.local>
2017-07-17 16:42:28 +0800

    打war包

--- src/main/java/com/huoqiu/controller/UserContactController.java
+++ src/main/java/com/huoqiu/controller/UserContactController.java
@@ -4,6 +4,7 @@ import javax.inject.Inject;
 
 
 import cn.huoqiu.base.db.DB;
+import cn.huoqiu.base.util.JsonResponse;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +13,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.huoqiu.entity.UserContact;
 import com.huoqiu.service.UserContactService;
-import cn.huoqiu.base.util.JsonResponse;
 import cn.huoqiu.base.web.BaseController;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/user_contacts")
@@ -31,5 +33,13 @@ public class UserContactController extends BaseController {
 		return  new JsonResponse().set("push",db.all(UserContact.class));
 //		return new JsonResponse().set("push",userContactService.findById((long) 1));
 	}
+
+//	@RequestMapping("getList")
+//	@ResponseBody
+//	public List<UserContact> getList(){
+//		return  db.all(UserContact.class);
+////		return new JsonResponse().set("push",userContactService.findById((long) 1));
+//	}
+
 
 }
